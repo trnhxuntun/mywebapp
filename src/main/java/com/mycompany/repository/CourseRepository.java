e5d347202004fa777c5591a0d6db424ec10139cd
trnhxuntun <trnhxuntun@gmail.com>
2022-08-19 23:39:39 +0700

    Revert "no message"
    This reverts commit ae272fdf906eabe9e3409818517c45fbc9950cad.

--- src/main/java/com/mycompany/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.repository;
-
-import com.mycompany.Model.Course;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, Integer> {
-}
